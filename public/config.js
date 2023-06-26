c86714034b9d7b2682370d3ea9efccfbc3237e37
jwilkesjr <jrjaygaming@gmail.com>
2023-06-25 22:26:57 -0700

    first commit

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://movie-web-app.jrjaygaming.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjZlOTgwMTEzNzA2YWVlYzdjMmY3OTkyZjZkMWI3MyIsInN1YiI6IjY0OTkxOGJjYmJkMGIwMDEwNjZmNjc5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1htz0iGgMGnItFLGf6pnJpzpi-wRkpzyteLYE8jaxmM"
 };
